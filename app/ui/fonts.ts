121581534e6fb6095ef9cca1ac201e596d9d22e8
Wunderay <60483832+wunderay@users.noreply.github.com>
2024-11-21 16:33:49 -0800

    fix build issue font.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,5 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: "400" });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" });
 
